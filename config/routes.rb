e00585f18ae1b5c7ce0755ae656288b8ff37c9a7
Joseph Levarato <Joseph Levarato>
2016-03-14 15:16:36 +0100

    Modif sur le routeur

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,6 @@
 Rails.application.routes.draw do
-
-  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  devise_for :users
-  resources :messages, only: [:new, :create]
-
   root :to => 'pages#accueil', :via => [:get, :post]
   match '/todo', :to => 'pages#todo', :via => [:get, :post]
   match '/about', :to => 'pages#about',   :via => [:get, :post]
-  match '/testadmin', :to => 'testadmin#index', :via => [:get, :post]
 
 end
