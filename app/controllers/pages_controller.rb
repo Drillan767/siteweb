ece68f635e9529cae9e6fe802791b259802a6277
Joseph Levarato <Joseph Levarato>
2016-03-14 15:13:37 +0100

    Suppression de toutes les infos relatives à la partie contact avant l'ajout d'une nouvelle branche spécifique

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,10 +3,6 @@ class PagesController < ApplicationController
     @titre = "Accueil"
   end
 
-  def contact
-    @titre = "Contact"
-  end
-
   def about
     @titre = "À propos"
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
 
-  resources :messages, only: [:new, :create]
+  resources :contacts, only: [:new, :create]
 
   root :to => 'pages#accueil', :via => [:get, :post]
   match '/todo', :to => 'pages#todo', :via => [:get, :post]
   match '/about', :to => 'pages#about',   :via => [:get, :post]
+ # match '/contact', :to => 'contacts#create', :via => :get
 
 end
